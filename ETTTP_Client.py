1f34870386d1e2ca4c64c925f81d7c4c8b2ffc55
Sohyun-B <90600397+Sohyun-B@users.noreply.github.com>
2023-06-02 22:01:39 +0900

    Update ETTTP_Client.py

--- ETTTP_Client.py
+++ ETTTP_Client.py
@@ -14,7 +14,47 @@ import tkinter as tk
 from socket import *
 import _thread
 
-from ETTTP_TicTacToe import TTT, check_msg
+def check_msg(msg, ip):
+    expected_ip="127.0.0.1"
+    try:
+        #message가 형식에 맞는지 확인
+        type, etttp, host, action=map(str, msg.split())
+
+        #ip가 제대로 작성되었는지 확인
+        if ip!=expected_ip:
+            return False
+
+        #send와 ack 맞는지 확인
+        if type not in ("SEND", "ACK", "RESULT"):
+            return False
+
+        #ETTTP이고, 버전이 맞는지 확인
+        if etttp!="ETTTP/1.0":
+            return False
+    
+        #호스트가 올바르고 구문이 맞는지 확인
+        if host!="Host:"+expected_ip:
+            return False
+        
+        #new move가 0과 2 사이이고 구문이 맞는지 확인
+        if type in ("SEND", "ACK") and action.startswith("New-Move:(") and action.endswith(")"):
+            a,b = map(str, action[10:-1].split(","))
+            if 0<=int(a)<=2 and 0<=int(b)<=2:
+                return True
+        elif type =="RESULT" and action.startswith("Winner:"):
+            winner= action[7:]
+            if winner in ("ME", "YOU"):
+                return True
+        elif type =="SEND" and action.startswith("First-Move:"):
+            first_turn=action[11:]
+            if first_turn in ("ME", "YOU"):
+                return True
+        return False
+    except Exception as e:
+        return False
+        
+
+#from ETTTP_TicTacToe_skeleton import TTT, check_msg
     
 if __name__ == '__main__':
 
@@ -31,18 +71,28 @@ if __name__ == '__main__':
         ###################################################################
         # Receive who will start first from the server
         start_move_message = client_socket.recv(SIZE).decode().strip()
-        check_reuslt = (check_msg(start_move_message, MY_IP))
+        check_result = (check_msg(start_move_message, MY_IP))
 
-        # Send ACK 
-        if check_reuslt:
+        start_index = start_move_message.index("First-Move:") + len("First-Move:")
+        start_user = start_move_message[start_index:]
+
+        if start_user=="ME": 
+            print("서버 선")
+            start=0
+        else: 
+            print("클라이언트 선")
+            start=1
+
+        # etttp에 맞는 형식인 것을 확인하고 Send ACK 
+        if check_result:
             ack_message ="ACK"+start_move_message[4:]
             client_socket.sendall(ack_message.encode())
         ###################################################################
         
         # Start game
-        #root = TTT(target_socket=client_socket, src_addr=MY_IP,dst_addr=SERVER_IP)
-        #root.play(start_user=start)
-        #root.mainloop()
+        # root = TTT(target_socket=client_socket, src_addr=MY_IP,dst_addr=SERVER_IP)
+        # root.play(start_user=start)
+        # root.mainloop()
         client_socket.close()
-        
+
         
